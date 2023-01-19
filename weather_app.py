e62d6081edfd1333553a1045f98808ec29607aa9
Kendi Rita <ritabkendi@gmail.com-MacBook-Pro.local>
2023-01-19 21:48:23 +0300

    changed paths

--- weather_app.py
+++ weather_app.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/home")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
