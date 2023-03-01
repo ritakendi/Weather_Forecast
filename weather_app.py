b100a2e324b1179007812e3476c842e7898a53df
Kendi Rita <ritabkendi@gmail.com-MacBook-Pro.local>
2023-03-01 21:54:50 +0300

    Add weather api application to get data from

--- weather_app.py
+++ weather_app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+def get_weather_results(zip_code, api_key):
+    api_url = https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 
 @app.route("/")
 @app.route("/home")
@@ -10,7 +13,7 @@ def index():
 
 
 @app.route("/homepage")
-def index():
+def homepage():
     return render_template("homepage.html")
 
 
